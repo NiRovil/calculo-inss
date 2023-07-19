24603e8bf85f4414a498ec4d36c1e67fbe985273
NiRovil <nicolas199851@hotmail.com>
2023-07-19 18:12:31 -0300

    first commit

---
+++ inss_calc.py
@@ -0,0 +1,7 @@
+import requests
+
+_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.188/dados?formato=json"
+
+class Inpc():
+
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from inss_calc import Inpc
+
+inicial = '07/1994'
+final = '06/2023'
+
+if __name__ == "__main__":
+    Inpc()
